d539ea3d23ffe260644913f2611b7efc9519128c
mrajani <mahesh@ionosolutions.com>
2018-11-15 11:55:59 -0800

    install zsh oh-my-zsh

---
+++ install_zsh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user=${1:-vagrant}
+
+if [[ ( -z ${os} && -z ${dist} ) ]]; then
+  if [ -e /etc/os-release ]; then
+    . /etc/os-release
+    os=$( echo ${ID} | awk '{ print tolower($1) }')
+    dist=${VERSION_ID}
+  fi
+fi
+echo "Found ${os} "
+
+if [[ ${os} == "centos" ]]; then
+  sudo yum -y install zsh
+elif [[ ${os} == "ubuntu" ]]; then
+  sudo apt-get -yqq update && sudo apt-get install -y zsh
+else
+  echo "Cannot identify the OS"
+  exit 1;
+fi
+
+sudo usermod -s /usr/bin/zsh ${user}
+curl -L http://install.ohmyz.sh | sh
+
