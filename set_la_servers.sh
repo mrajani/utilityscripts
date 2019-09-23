8e10386cc68a966c2b7647673c43f8a14daf723b
mrajani <mahesh@ionosolutions.com>
2019-09-22 17:53:42 -0700

    optimize the script with sshpass

--- set_la_servers.sh
+++ set_la_servers.sh
@@ -4,6 +4,7 @@
 [[ -z $LAPASSWD ]] && echo "LAPASSWD is not set" && exit
 [[ -z $MYHOST ]]   && echo "MYHOST is not set" && exit
 [[ -z $DOMAIN ]]   && echo "DOMAIN is not set" && exit
+export SSHPASS=${LAPASSWD}
 
 for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
   ssh-keygen -f /home/vagrant/.ssh/known_hosts -R $server
@@ -12,16 +13,15 @@ done
 
 for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
   echo run ssh-copy-id
-  sshpass -p $LAPASSWD ssh-copy-id -i ~/.ssh/lnxa_rsa.pub $server:
-done
-
-for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
+  sshpass -e ssh-copy-id -i ~/.ssh/lnxa_rsa.pub $server:
   scp ~/.gitconfig ~/.ssh/config ~/.ssh/git_id_ecdsa* \
     ~/.ssh/id_rsa* ~/.ssh/lnxa_rsa* ${server}:.ssh
 done
 
 for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
     echo set up cloud user for sudo privileges
-    sshpass -p $LAPASSWD echo "$LAPASSWD" | ssh -qt ${server} sudo -S cp /etc/sudoers.d/{ssm-agent-users,cloud_user}
-    sshpass -p $LAPASSWD echo "$LAPASSWD" | ssh -qt ${server} sudo -S sed -i 's/ssm-user/cloud_user/' /etc/sudoers.d/cloud_user
+    sshpass -e echo "$LAPASSWD" | \
+       ssh -qt ${server} sudo -S cp /etc/sudoers.d/{ssm-agent-users,cloud_user}
+    sshpass -e echo "$LAPASSWD" | \
+       ssh -qt ${server} sudo -S sed -i 's/ssm-user/cloud_user/' /etc/sudoers.d/cloud_user
 done
