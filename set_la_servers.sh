bc2bf9dda6598376c2b180b491a0e75979f1fdf0
mrajani <mahesh@ionosolutions.com>
2019-09-26 15:47:21 +0000

    bash script to setup servers on la academy

--- set_la_servers.sh
+++ set_la_servers.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 # copy req files to server on lnx academy
+# the ~/.ssh/config has the default user and ssh keys
 
 [[ -z $LAPASSWD ]] && echo "LAPASSWD is not set" && exit
 [[ -z $MYHOST ]]   && echo "MYHOST is not set" && exit
 [[ -z $DOMAIN ]]   && echo "DOMAIN is not set" && exit
 export SSHPASS=${LAPASSWD}
 
-for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
+[[ ! -f ~/.ssh/lnxa_rsa ]] && \
+   ssh-keygen -t rsa -b 2048 -N "" -C "lnx-academy" -f ~/.ssh/lnxa_rsa
+
+
+for server in ${MYHOST}{1..9}c.${DOMAIN}; do 
   ssh-keygen -f /home/vagrant/.ssh/known_hosts -R $server
   ssh-keyscan ${server} >> $HOME/.ssh/known_hosts
-done
 
-for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
   echo run ssh-copy-id
   sshpass -e ssh-copy-id -i ~/.ssh/lnxa_rsa.pub $server:
   scp ~/.gitconfig ~/.ssh/config ~/.ssh/git_id_ecdsa* \
     ~/.ssh/id_rsa* ~/.ssh/lnxa_rsa* ${server}:.ssh
 done
 
-for server in ${MYHOST}{1..6}c.${DOMAIN}; do 
+
+for server in ${MYHOST}{1..9}c.${DOMAIN}; do 
     echo set up cloud user for sudo privileges
     sshpass -e echo "$LAPASSWD" | \
-       ssh -qt ${server} sudo -S cp /etc/sudoers.d/{ssm-agent-users,cloud_user}
+       ssh -qt ${server} sudo -S echo "cloud_user ALL=\(ALL\) NOPASSWD:ALL | sudo tee /etc/sudoers.d/cloud_user"
     sshpass -e echo "$LAPASSWD" | \
-       ssh -qt ${server} sudo -S sed -i 's/ssm-user/cloud_user/' /etc/sudoers.d/cloud_user
+       ssh -qt ${server} sudo -S echo "ansible ALL=\(ALL\) NOPASSWD:ALL | sudo tee /etc/sudoers.d/ansible"
 done
