6ec833717ae8ca632f60dd4e74c93f273e2a858d
mrajani <laltopi@gmail.com>
2020-12-07 07:59:20 -0800

    create certs with subject alt names sans

--- gen_ca.sh
+++ gen_ca.sh
@@ -2,10 +2,21 @@
 
 #---- Script to Generate Self Signed Certs ----#
 
-hostname=`hostname -s`
+hostname=`hostname -f`
 openssl genrsa -out ${hostname}.key 2048
 openssl req -new -key ${hostname}.key -out ${hostname}.csr \
   -extensions v3_ca \
   -subj "/C=US/ST=CA/O=JustDoIt/L=San Jose/OU=JustDoIt/CN=Universal CA root"
 openssl x509 -req -days 90 -in ${hostname}.csr -signkey ${hostname}.key \
   -out ${hostname}.crt
+
+HOSTNAME=`hostname -s`
+IP_ADDR=$(hostname -I | cut -d " " -f2)
+openssl req -x509 -sha256 -nodes \
+  -newkey rsa:4096 -days 90 \
+  -keyout ./${HOSTNAME}.key -out ./${HOSTNAME}.crt \
+  -subj "/CN=${HOSTNAME}" \
+  -extensions san \
+  # -config <(cat /etc/ssl/openssl.cnf <(echo -e "\n[san]\nsubjectAltName=DNS:${HOSTNAME},IP:$IP_ADDR"))
+  -config <(echo -e "\n[san]\nsubjectAltName=DNS:${HOSTNAME},IP:$IP_ADDR")
+
