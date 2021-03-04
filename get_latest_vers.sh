00893af3a1ef287d998b68824a7a7cc2b13239ae
mrajani <laltopi@gmail.com>
2021-03-03 22:06:51 -0800

    enhance with checkpoint-api call to hashicorp for latest version of products

--- get_latest_vers.sh
+++ get_latest_vers.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # Show latest version of Hashicorp
 
+url=https://checkpoint-api.hashicorp.com/v1/check/nomad
+ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
+echo nomad $ver
+
+url=https://checkpoint-api.hashicorp.com/v1/check/vagrant
+ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
+echo vagrant $ver
+
+url=https://checkpoint-api.hashicorp.com/v1/check/packer
+ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
+echo packer $ver
 
 latest_url=https://github.com/hashicorp/packer/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 packer_version=${temp##*/v}
 echo packer $packer_version
+
+url=https://checkpoint-api.hashicorp.com/v1/check/consul
+ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
+echo consul $ver
 
 latest_url=https://github.com/hashicorp/consul/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
@@ -22,9 +37,13 @@ temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 terraform_version=${temp##*/v}
 echo terraform $terraform_version
 
+url=https://checkpoint-api.hashicorp.com/v1/check/terraform
+ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
+echo terraform $ver
+
 latest_url=https://github.com/ericchiang/pup/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 pup_version=${temp##*/v}
 echo pup $pup_version
 #cat consul.html | pup 'body ul li a text{}' | egrep -v '\.\.|ent|rc|beta' | head -1
-
+#cat vault.html  | pup 'body ul li a text{}' | egrep -v '\.\.|ent|rc|beta' | head -1
