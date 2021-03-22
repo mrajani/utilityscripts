9452a8b2d88c9a0fa4a6ba601335b986ed55df7d
mrajani <laltopi@gmail.com>
2021-03-22 16:28:55 -0700

    instal vs code extensions

--- install_vscode_ext.sh
+++ install_vscode_ext.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v code)" ]; then
+code=$(command -v codes-server || command -v code)
+
+if [ -z $code ]; then
   echo "visual studio code is not installed" >&2
   exit 1
 fi
 
-code --install-extension eamodio@gitlens
-code --install-extension eamodio.gitlens
-code --install-extension hashicorp.terraform
-code --install-extension esbenp.prettier-vscode
-code --install-extension coenraads.bracket-pair-colorizer-2
-code --install-extension oderwat.indent-rainbow
-code --install-extension zbr.vscode-ansible
-code --install-extension pkief.material-icon-theme
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension redhat.vscode-yaml
-code --install-extension christian-kohler.path-intellisense
-code --install-extension ms-vscode.azurecli
-code --install-extension davidanson.vscode-markdownlint
+$code --install-extension eamodio.gitlens
+$code --install-extension hashicorp.terraform
+$code --install-extension esbenp.prettier-vscode
+$code --install-extension coenraads.bracket-pair-colorizer-2
+$code --install-extension oderwat.indent-rainbow
+$code --install-extension zbr.vscode-ansible
+$code --install-extension pkief.material-icon-theme
+$code --install-extension ms-azuretools.vscode-docker
+$code --install-extension redhat.vscode-yaml
+$code --install-extension christian-kohler.path-intellisense
+$code --install-extension ms-vscode.azurecli
+$code --install-extension davidanson.vscode-markdownlint
 
