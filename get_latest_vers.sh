8fc4c858030550fe133ad8ff7f9cbee528a2110a
mrajani <laltopi@gmail.com>
2021-05-18 14:04:33 -0700

    use api.github url to get latest versions of hashicorp products

--- get_latest_vers.sh
+++ get_latest_vers.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 # Show latest version of Hashicorp
 
+#----------- Ansible -----------#
 url=https://github.com/ansible/awx/releases/latest
 temp=$(curl -sSL -I -o /dev/null ${url} -w %{url_effective})
 awx_version=${temp##*/}
 echo Ansible AWX tag ${awx_version}
 
+#----------- Nomad -----------#
 url=https://checkpoint-api.hashicorp.com/v1/check/nomad
 ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
 echo nomad $ver
 
+url=https://api.github.com/repos/hashicorp/nomad/releases/latest
+ver=$(curl -sL ${url} | jq -r ".name")
+echo nomad $ver
+
+#----------- Vagrant -----------#
 url=https://checkpoint-api.hashicorp.com/v1/check/vagrant
 ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
 echo vagrant $ver
 
+
+#----------- Packer -----------#
 url=https://checkpoint-api.hashicorp.com/v1/check/packer
 ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
 echo packer $ver
@@ -23,6 +32,11 @@ temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 packer_version=${temp##*/v}
 echo packer $packer_version
 
+url=https://api.github.com/repos/hashicorp/packer/releases/latest
+ver=$(curl -sL ${url} | jq -r ".name")
+echo packer $ver
+
+#----------- Consul -----------#
 url=https://checkpoint-api.hashicorp.com/v1/check/consul
 ver=$(curl -sSL  ${url} | jq '.current_version'| sed -e 's/"//g')
 echo consul $ver
@@ -32,11 +46,23 @@ temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 consul_version=${temp##*/v}
 echo consul $consul_version
 
+url=https://api.github.com/repos/hashicorp/consul/releases/latest
+ver=$(curl -sL ${url} | jq -r ".name")
+echo consul $ver
+
+#----------- Vault -----------#
 latest_url=https://github.com/hashicorp/vault/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 vault_version=${temp##*/v}
 echo vault $vault_version
 
+
+url=https://api.github.com/repos/hashicorp/vault/releases/latest
+ver=$(curl -sL ${url} | jq -r ".name")
+echo vault $ver
+
+
+#----------- Vault -----------#
 latest_url=https://github.com/hashicorp/terraform/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
 terraform_version=${temp##*/v}
@@ -44,7 +70,11 @@ echo terraform $terraform_version
 
 url=https://checkpoint-api.hashicorp.com/v1/check/terraform
 ver=$(curl -sSL  ${url} | jq -r '.current_version')
-echo terraform $ver -
+echo terraform $ver
+
+url=https://api.github.com/repos/hashicorp/terraform/releases/latest
+ver=$(curl -sL ${url} | jq -r ".name")
+echo terraform $ver
 
 latest_url=https://github.com/ericchiang/pup/releases/latest
 temp=$(curl -sSL -I -o /dev/null -w %{url_effective} $latest_url)
