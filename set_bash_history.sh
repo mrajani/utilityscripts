b1b166fd7e92f463a539a9153fc4a9476fe9488b
mrajani <laltopi@gmail.com>
2021-08-08 22:26:42 -0700

    remove dupes in history, common cmds

--- set_bash_history.sh
+++ set_bash_history.sh
@@ -1,5 +1,5 @@
 # ignore common commands
-echo export HISTIGNORE=”exit:pwd:rm:cp:cd:ls:clear:history:” >> ~/.bashrc
+echo export HISTIGNORE=”vi:exit:pwd:rm:cp:cd:ls:clear:history:” >> ~/.bashrc
 # ignore duplicate entries
 echo export HISTCONTROL=ignoredups >> ~/.bashrc
 # echo shopt -s histappend >> ~/.bashrc
