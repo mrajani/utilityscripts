5814b8239cd6839ecf1bc5090307fab4cfff37ca
mrajani <laltopi@gmail.com>
2021-09-06 21:01:20 -0700

    delete this script, replaced by install_hc_product

--- install_vault.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install Vault Server on Linux Platform 
-
-tag=${1:-1.5.0}
-vault=vault_${tag}_linux_amd64.zip
-url=https://releases.hashicorp.com/vault/${tag}/${vault}
-echo "Downloading ${tag} version of Vault"
-wget -c -q ${url} && unzip -d ~/.local/bin ${vault} && rm ${vault} 
-
